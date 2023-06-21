5157083826018b38162e35d81604189ac69aaa53
AbinavRavi <subramathreya@gmail.com>
2023-06-21 19:20:24 +0200

    added preprocess text

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,7 +1,28 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import string
 
 def separate_reviews(text):
     reviews = text.split("\n")
     return reviews, len(reviews)
 
-def preprocess():
-    pass
+def preprocess_text(text):
+    # Tokenization
+    tokens = word_tokenize(text.lower())
+    
+    # Remove punctuation
+    tokens = [token for token in tokens if token not in string.punctuation]
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+    # Join tokens back into a single string
+    preprocessed_text = ' '.join(tokens)
+    
+    return preprocessed_text
